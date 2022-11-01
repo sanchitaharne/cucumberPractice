6b0fc9f2e237c51994c3082d71921ce2937f4fb0
sanchita <sanchita.241@gmail.com>
2022-11-01 10:15:50 +0530

    commit

--- src/test/java/org/cucumber/learning/features/datatables.feature
+++ src/test/java/org/cucumber/learning/features/datatables.feature
@@ -13,3 +13,12 @@ Feature: Data table
       |Cucumber Chutney|1|10|
     When I generate the bill
     Then a bill of $50 should be generated
+
+  @listofmaps
+  Scenario: Bill amount generation
+    Given I placed an order for the following items
+      |ItemName|Units|UnitPrice|
+      |Cucumber pizza|2|20|
+      |Cucumber Chutney|1|10|
+    When I generate the bill
+    Then a bill of $50 should be generated
--- src/test/java/stepDefinitions/DataTableSteps.java
+++ src/test/java/stepDefinitions/DataTableSteps.java
@@ -1,18 +1,19 @@
 package stepDefinitions;
 
+import java.util.Map;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-
 import java.util.List;
-
 public class DataTableSteps {
-
     @Given("I placed an order for the following items")
     public void i_placed_an_order_for_the_following_items(io.cucumber.datatable.DataTable dataTable) {
-        List<List<String>> billItems = dataTable.asLists(String.class);
-        for (List<String> billItem: billItems)
-            System.out.println(billItem);
+        List<Map<String, String>> billItems = dataTable.asMaps(String.class, String.class);
+        for (Map<String, String> billItem: billItems)
+            for(Map.Entry<String, String> item: billItem.entrySet()){
+                System.out.println("Key- "+item.getKey());
+                System.out.println("Value- "+item.getValue());
+            }
 
 /*        for (List<String> billItem: billItems) {
          for(String s:billItem) {
